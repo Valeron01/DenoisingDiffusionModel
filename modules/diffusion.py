d46736a5446dd3f6ec0d9174ecb9e93e9e89bf7b
Valeron01 <electronpochta3011@mail.ru>
2023-07-10 03:07:47 +0300

    Added inference

--- modules/diffusion.py
+++ modules/diffusion.py
@@ -2,6 +2,7 @@ import math
 
 import torch
 from torch import nn
+from tqdm import trange
 
 
 def cosine_beta_schedule(num_steps, s=0.008):
@@ -24,7 +25,7 @@ class Diffusion(nn.Module):
         betas = cosine_beta_schedule(num_steps)
         alphas = 1 - betas
         alpha_hat = torch.cumprod(alphas, 0)
-
+        self.num_steps = num_steps
         self.register_buffer("alpha_hat", alpha_hat)
 
     def noise_images(self, images, t: torch.Tensor):
@@ -34,4 +35,30 @@ class Diffusion(nn.Module):
         noised_images = images * torch.sqrt(t) + noise * torch.sqrt(1 - t)
 
         return noised_images, noise
+
+    def sample(self, model, noises, classes):
+        assert noises.shape[0] == classes.shape[0]
+        betas = cosine_beta_schedule(self.num_steps).to(noises.device)
+        alphas = 1 - betas
+        alpha_hat = torch.cumprod(alphas, 0)
+
+        with torch.no_grad():
+            for i in trange(self.num_steps - 1, 0, -1):
+                t = torch.full([classes.shape[0]], i, device=classes.device, dtype=torch.long)
+                predicted_noise = model(noises, t, classes)
+
+                epsilon = torch.randn_like(noises) if i > 1 else 0
+
+                noises = 1 / torch.sqrt(alphas[i]) * (
+                        noises - ((1 - alphas[i]) / torch.sqrt(1 - alpha_hat[i])) * predicted_noise
+                ) + epsilon * torch.sqrt(betas[i])
+
+        return noises
+
+
+
+
+
+
+
 
